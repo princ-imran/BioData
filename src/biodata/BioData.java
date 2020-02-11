baa5ae8201d0e1340f1f005a1806589fa5c4ae46
Imran Hoshain <princ.imran@gmail.com>
2020-02-12 00:33:24 +0600

    Java Basic

---
+++ src/biodata/BioData.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata;
+
+/**
+ *
+ * @author Spy Hacker
+ */
+public class BioData {
+    public static void main(String arg[]){
+        System.out.print("Name: Imran Hoshain \nAge:26 \nStudy: Canadian University of Bangladesh");
+    }
+}
